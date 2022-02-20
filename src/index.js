59e148bca6f6e1d6fbebb7c3b4ec10de44e23c91
sharapovk <sharapovk@gmail.com>
2022-02-20 14:34:16 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+function reverse(num) {
+    let tempNum = Math.abs(num);
+    let result = "";
+
+    while (tempNum) {
+        result += tempNum % 10;
+        tempNum = ~~(tempNum / 10);
+    }
+
+    return +result;
 }
+
+module.exports = reverse;
